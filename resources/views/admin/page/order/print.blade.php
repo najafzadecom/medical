1c3b04e5a0d0b1bff5c89a67c86df0e83fbd7811
nadjafzadeh@gmail.com <!@y1y2y3!@>
2021-12-31 16:25:37 +0400

    1

--- resources/views/admin/page/order/print.blade.php
+++ resources/views/admin/page/order/print.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Print</title>
 </head>
 <body>
-<img width="150" src="data:image/png;base64, {!! DNS1D::getBarcodePNG($order->number, 'C39', 2, 30, array(0, 0, 0), true) !!}"/>
+{!! DNS1D::getBarcodeSVG($order->number, 'C39') !!}
 <script type="text/javascript">
-    window.onload = function() { window.print();window.close(); }
+    window.onload = function() { window.print(); }
 </script>
 </body>
 </html>
